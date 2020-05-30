0a9015d8dd87f608ee9d2f4d6fc544d8f31f6ae0
Philo van Kemenade <philovankemenade@gmail.com>
2020-05-30 22:09:03 +0200

    add SU typeform

---
+++ pages/storytellersunited/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <iframe
+      id="typeform-full"
+      width="100%"
+      height="100%"
+      frameborder="0"
+      allow="camera; microphone; autoplay; encrypted-media;"
+      src="https://gunnardejong.typeform.com/to/nqEQHQ"
+    ></iframe>
+    <script
+      type="text/javascript"
+      src="https://embed.typeform.com/embed.js"
+    ></script>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import MCSignupForm from "~/components/MCSignupForm.vue";
+
+export default {
+  components: {
+    Logo,
+    MCSignupForm,
+  },
+};
+</script>
+
+<style>
+html {
+  margin: 0;
+  height: 100%;
+  overflow: hidden;
+}
+iframe {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  border: 0;
+}
+</style>
