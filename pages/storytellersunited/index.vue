673a51513d429417a3d81692e81fb05f89e118f4
Philo van Kemenade <philovankemenade@gmail.com>
2020-10-02 22:21:29 +0200

    add previous sessions section

--- pages/storytellersunited/index.vue
+++ pages/storytellersunited/index.vue
@@ -1,39 +1,85 @@
 <template>
   <div class="bg-su-washed-orange">
-    <nav class="bg-su-dark-orange pa3">
+    <nav class="bg-su-light-orange pa3">
       <nuxt-link to="/">
         <logo class="w3" />
       </nuxt-link>
     </nav>
     <div class="tc pa4">
-      <img class="w4 mv4" src="https://storytellers.link/img/logo-storytellersunited-200px.png" alt="Storytellers United Logo">
-      <div>
-        <h1 class="measure center">
-          Hi 👋 <br>
-           Welcome to Storytellers United on Gradual!
-        </h1>
-        <p class="f4 measure center">This is a space to express intentions for things you’d like to learn and share with others in the community.</p>
-      </div>
-      <div class="mv5">
-        <h2 class="mb3">Ready to start learning together?</h2>
-        <nuxt-link
-          to="/storytellersunited/join"
-          class="f4 link grow br3 ph3 pv2 mb2 dib white bg-dark-green"
-        >
-          Join the group
-        </nuxt-link>
-      </div>
-      <div class="mb4 measure center">
-        <h3 class="mb3 f4">
-          Already joined & curious to see what others would like to learn and share?
-        </h3>
-        <nuxt-link
-          to="/storytellersunited/map"
-          class="f4 dark-green"
-        >
-          See group skills
-        </nuxt-link>
-      </div>
+      <section>
+        <div class="mv4">
+          <a href="https://storytellers.link/" target="_blank">
+            <img class="w4" src="https://storytellers.link/img/logo-storytellersunited-200px.png" alt="Storytellers United Logo">
+          </a>
+        </div>
+        <div>
+          <h1 class="measure center">
+            Hi 👋 <br>
+             Welcome to Storytellers United on Gradual!
+          </h1>
+          <p class="f4 fw3 measure center">This is a space to express intentions for things you’d like to learn and share with others in the community.</p>
+          <nuxt-link
+            to="/storytellersunited/join"
+            class="mv4 f4 link grow br3 ph3 pv2 dib white bg-dark-green"
+          >
+            Join the group
+          </nuxt-link>
+          <h2 class="mb3 f4 fw3">
+            Already joined & curious to see what others would like to learn and share?
+          </h2>
+          <nuxt-link
+            to="/storytellersunited/map"
+            class="f4 dark-green"
+          >
+            See group skills
+          </nuxt-link>
+        </div>
+      </section>
+      <section class="mv5">
+        <h2>Things we learned in previous sessions</h2>
+        <ul class="list pa0 ma0 flex flex-wrap">
+          <li class="center w-100 w-50-m mw8 pa3">
+              <article class="tl bg-white-80 br3 ba-hover-dark-blue">
+                  <div class="flex flex-column flex-row-l">
+                    <div class="w-100 w-40-l">
+                    <a href="https://www.youtube.com/watch?v=qI8W30t4bIM" target="_blank" class="db no-underline black w-100 h-100">
+                      <img src="~/assets/su-how_to_handle.jpg" alt="screenshot of a virtual lunch and learn session featuring a wheel of emotions" class="db w-100 h-100 br3  br--top-nl br--left-l objfit-cover">
+                    </a>
+                    </div>
+                    <div class="w-100 w-60-l pa3">
+                      <div class="flex items-center justify-between flex-wrap">
+                        <span class="f7 bg-light-green dark-green ph2 pv1 mv1 dib br-pill">lunch & learn</span>
+                        <span class="o-70">30 Sept 2020</span>
+                      </div>
+                      <h3 class="mt1 lh-title">How to handle conversations with xenophobes and racists?</h3>
+                      <div class="lh-copy measure mt2">
+                        <h3 class="f5">learned by</h3>
+                        <ul class="flex list pa0">
+                          <li>             <img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-UD1UZ0J3H-573c02ead937-32" alt="profile picture of Gunnar de Jong"></li>
+                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-U14SLMJ20-ab75d1919845-32" alt="profile picture of Philo"></li>
+                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-UQDCZCT7F-g26743a83b8d-32" alt="profile picture of hay"></li>
+                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-UU4SUJ9U2-gf27c210f8aa-32" alt="profile picture of Kevin"></li>
+                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-U014ACSTHNJ-7b4a05e8e80b-32" alt="profile picture of 1coko"></li>
+                        </ul>
+                        <h3 class="f5">shared by</h3>
+                        <ul class="flex list pa0">
+                          <li>             <img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-UAX2YQV2R-8ab84e1fbcff-32" alt="profile picture of Anna Desponds"></li>
+                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-U01BCG23VM3-bb24210db841-32" alt="profile picture of Gosia"></li>
+                        </ul>
+                        <h3 class="f5">resources</h3>
+                        <ul class="list pa0">
+                          <!-- <li><a class="dark-green f6" href="https://www.youtube.com/watch?v=qI8W30t4bIM">session recording</a></li> -->
+                          <!-- <li><a class="dark-green f6" href="https://docs.google.com/document/d/1faJe-eDTFZZEC1PATqmoecQw_Qx60XkolRrQsMH7TZc/edit#">note document</a></li> -->
+                          <a class="dark-green f6" href="https://www.youtube.com/watch?v=qI8W30t4bIM">session recording</a>, 
+                          <a class="dark-green f6" href="https://docs.google.com/document/d/1faJe-eDTFZZEC1PATqmoecQw_Qx60XkolRrQsMH7TZc/edit#">note document</a>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+              </article>
+          </li>
+        </ul>
+        </section>
     </div>
   </div>
 </template>
@@ -68,4 +114,17 @@ export default {
 .bg-su-light-blue {    background-color: var(--color-su-light-blue);}
 .bg-su-dark-blue {     background-color: var(--color-su-dark-blue);}
 html { background-color: var(--color-su-washed-orange);}
+
+@media (max-width: 60em) {
+  .br--top-nl {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+}
+.objfit-cover  {
+  object-fit: cover;
+}
+.ml-2 {
+  margin-left: -0.5rem;
+}
 </style>
