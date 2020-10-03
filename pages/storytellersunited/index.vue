feccf251725ee238f2983cc73f3208ff7831eaa7
Philo van Kemenade <philovankemenade@gmail.com>
2020-10-03 13:54:39 +0200

    format code

--- pages/storytellersunited/index.vue
+++ pages/storytellersunited/index.vue
@@ -9,15 +9,22 @@
       <section>
         <div class="mv4">
           <a href="https://storytellers.link/" target="_blank">
-            <img class="w4" src="https://storytellers.link/img/logo-storytellersunited-200px.png" alt="Storytellers United Logo">
+            <img
+              class="w4"
+              src="https://storytellers.link/img/logo-storytellersunited-200px.png"
+              alt="Storytellers United Logo"
+            />
           </a>
         </div>
         <div>
           <h1 class="measure center">
-            Hi 👋 <br>
-             Welcome to Storytellers United on Gradual!
+            Hi 👋 <br />
+            Welcome to Storytellers United on Gradual!
           </h1>
-          <p class="f4 fw3 measure center">This is a space to express intentions for things you’d like to learn and share with others in the community.</p>
+          <p class="f4 fw3 measure center">
+            This is a space to express intentions for things you’d like to learn
+            and share with others in the community.
+          </p>
           <nuxt-link
             to="/storytellersunited/join"
             class="mv4 f4 link grow br3 ph3 pv2 dib white bg-dark-green"
@@ -25,12 +32,10 @@
             Join the group
           </nuxt-link>
           <h2 class="mb3 f4 fw3">
-            Already joined & curious to see what others would like to learn and share?
+            Already joined & curious to see what others would like to learn and
+            share?
           </h2>
-          <nuxt-link
-            to="/storytellersunited/map"
-            class="f4 dark-green"
-          >
+          <nuxt-link to="/storytellersunited/map" class="f4 dark-green">
             See group skills
           </nuxt-link>
         </div>
@@ -39,47 +44,108 @@
         <h2>Things we've learned in previous sessions</h2>
         <ul class="list pa0 ma0 flex flex-wrap">
           <li class="center w-100 w-50-m mw8 pa3">
-              <article class="tl bg-white-80 br3 ba-hover-dark-blue">
-                  <div class="flex flex-column flex-row-l">
-                    <div class="w-100 w-40-l">
-                    <a href="https://www.youtube.com/watch?v=qI8W30t4bIM" target="_blank" class="db no-underline black w-100 h-100">
-                      <img src="~/assets/su-how_to_handle.jpg" alt="screenshot of a virtual lunch and learn session featuring a wheel of emotions" class="db w-100 h-100 br3  br--top-nl br--left-l objfit-cover">
-                    </a>
-                    </div>
-                    <div class="w-100 w-60-l pa3">
-                      <div class="flex items-center justify-between flex-wrap">
-                        <span class="f7 bg-light-green dark-green ph2 pv1 mv1 dib br-pill">lunch & learn</span>
-                        <span class="o-70">30 Sept 2020</span>
-                      </div>
-                      <h3 class="mt1 lh-title">How to handle conversations with xenophobes and racists?</h3>
-                      <div class="lh-copy measure mt2">
-                        <h3 class="f5">learned by</h3>
-                        <ul class="flex list pa0">
-                          <li>             <img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-UD1UZ0J3H-573c02ead937-32" alt="profile picture of Gunnar de Jong"></li>
-                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-U14SLMJ20-ab75d1919845-32" alt="profile picture of Philo"></li>
-                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-UQDCZCT7F-g26743a83b8d-32" alt="profile picture of hay"></li>
-                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-UU4SUJ9U2-gf27c210f8aa-32" alt="profile picture of Kevin"></li>
-                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-U014ACSTHNJ-7b4a05e8e80b-32" alt="profile picture of 1coko"></li>
-                        </ul>
-                        <h3 class="f5">shared by</h3>
-                        <ul class="flex list pa0">
-                          <li>             <img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-UAX2YQV2R-8ab84e1fbcff-32" alt="profile picture of Anna Desponds"></li>
-                          <li class="ml-2"><img class="br-100 w2 dib ba b--white bw2" src="https://ca.slack-edge.com/T14SUV8BA-U01BCG23VM3-bb24210db841-32" alt="profile picture of Gosia"></li>
-                        </ul>
-                        <h3 class="f5">resources</h3>
-                        <ul class="list pa0">
-                          <!-- <li><a class="dark-green f6" href="https://www.youtube.com/watch?v=qI8W30t4bIM">session recording</a></li> -->
-                          <!-- <li><a class="dark-green f6" href="https://docs.google.com/document/d/1faJe-eDTFZZEC1PATqmoecQw_Qx60XkolRrQsMH7TZc/edit#">note document</a></li> -->
-                          <a class="dark-green f6" href="https://www.youtube.com/watch?v=qI8W30t4bIM">session recording</a>, 
-                          <a class="dark-green f6" href="https://docs.google.com/document/d/1faJe-eDTFZZEC1PATqmoecQw_Qx60XkolRrQsMH7TZc/edit#">note document</a>
-                        </ul>
-                      </div>
-                    </div>
+            <article class="tl bg-white-80 br3 ba-hover-dark-blue">
+              <div class="flex flex-column flex-row-l">
+                <div class="w-100 w-40-l">
+                  <a
+                    href="https://www.youtube.com/watch?v=qI8W30t4bIM"
+                    target="_blank"
+                    class="db no-underline black w-100 h-100"
+                  >
+                    <img
+                      src="~/assets/su-how_to_handle.jpg"
+                      alt="screenshot of a virtual lunch and learn session featuring a wheel of emotions"
+                      class="db w-100 h-100 br3  br--top-nl br--left-l objfit-cover"
+                    />
+                  </a>
+                </div>
+                <div class="w-100 w-60-l pa3">
+                  <div class="flex items-center justify-between flex-wrap">
+                    <span
+                      class="f7 bg-light-green dark-green ph2 pv1 mv1 dib br-pill"
+                      >lunch & learn</span
+                    >
+                    <span class="o-70">30 Sept 2020</span>
                   </div>
-              </article>
+                  <h3 class="mt1 lh-title">
+                    How to handle conversations with xenophobes and racists?
+                  </h3>
+                  <div class="lh-copy measure mt2">
+                    <h3 class="f5">learned by</h3>
+                    <ul class="flex list pa0">
+                      <li>
+                        <img
+                          class="br-100 w2 dib ba b--white bw2"
+                          src="https://ca.slack-edge.com/T14SUV8BA-UD1UZ0J3H-573c02ead937-32"
+                          alt="profile picture of Gunnar de Jong"
+                        />
+                      </li>
+                      <li class="ml-2">
+                        <img
+                          class="br-100 w2 dib ba b--white bw2"
+                          src="https://ca.slack-edge.com/T14SUV8BA-U14SLMJ20-ab75d1919845-32"
+                          alt="profile picture of Philo"
+                        />
+                      </li>
+                      <li class="ml-2">
+                        <img
+                          class="br-100 w2 dib ba b--white bw2"
+                          src="https://ca.slack-edge.com/T14SUV8BA-UQDCZCT7F-g26743a83b8d-32"
+                          alt="profile picture of hay"
+                        />
+                      </li>
+                      <li class="ml-2">
+                        <img
+                          class="br-100 w2 dib ba b--white bw2"
+                          src="https://ca.slack-edge.com/T14SUV8BA-UU4SUJ9U2-gf27c210f8aa-32"
+                          alt="profile picture of Kevin"
+                        />
+                      </li>
+                      <li class="ml-2">
+                        <img
+                          class="br-100 w2 dib ba b--white bw2"
+                          src="https://ca.slack-edge.com/T14SUV8BA-U014ACSTHNJ-7b4a05e8e80b-32"
+                          alt="profile picture of 1coko"
+                        />
+                      </li>
+                    </ul>
+                    <h3 class="f5">shared by</h3>
+                    <ul class="flex list pa0">
+                      <li>
+                        <img
+                          class="br-100 w2 dib ba b--white bw2"
+                          src="https://ca.slack-edge.com/T14SUV8BA-UAX2YQV2R-8ab84e1fbcff-32"
+                          alt="profile picture of Anna Desponds"
+                        />
+                      </li>
+                      <li class="ml-2">
+                        <img
+                          class="br-100 w2 dib ba b--white bw2"
+                          src="https://ca.slack-edge.com/T14SUV8BA-U01BCG23VM3-bb24210db841-32"
+                          alt="profile picture of Gosia"
+                        />
+                      </li>
+                    </ul>
+                    <h3 class="f5">resources</h3>
+                    <ul class="list pa0">
+                      <a
+                        class="dark-green f6"
+                        href="https://www.youtube.com/watch?v=qI8W30t4bIM"
+                        >session recording</a
+                      >,
+                      <a
+                        class="dark-green f6"
+                        href="https://docs.google.com/document/d/1faJe-eDTFZZEC1PATqmoecQw_Qx60XkolRrQsMH7TZc/edit#"
+                        >note document</a
+                      >
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </article>
           </li>
         </ul>
-        </section>
+      </section>
     </div>
   </div>
 </template>
@@ -100,20 +166,36 @@ export default {
 
 <style>
 :root {
-  --color-su-washed-orange: #FDECCE;
-  --color-su-light-orange: #F4A726;
-  --color-su-dark-orange: #FF5A17;
-  --color-su-light-blue: #C3ECF7;
-  --color-su-dark-blue: #02ACE3;
+  --color-su-washed-orange: #fdecce;
+  --color-su-light-orange: #f4a726;
+  --color-su-dark-orange: #ff5a17;
+  --color-su-light-blue: #c3ecf7;
+  --color-su-dark-blue: #02ace3;
 }
-.su-light-orange {     color:            var(--color-su-light-orange);}
-.su-dark-orange {      color:            var(--color-su-dark-orange);}
-.bg-su-light-orange {  background-color: var(--color-su-light-orange);}
-.bg-su-dark-orange {   background-color: var(--color-su-dark-orange);}
-.bg-su-washed-orange { background-color: var(--color-su-washed-orange);}
-.bg-su-light-blue {    background-color: var(--color-su-light-blue);}
-.bg-su-dark-blue {     background-color: var(--color-su-dark-blue);}
-html { background-color: var(--color-su-washed-orange);}
+.su-light-orange {
+  color: var(--color-su-light-orange);
+}
+.su-dark-orange {
+  color: var(--color-su-dark-orange);
+}
+.bg-su-light-orange {
+  background-color: var(--color-su-light-orange);
+}
+.bg-su-dark-orange {
+  background-color: var(--color-su-dark-orange);
+}
+.bg-su-washed-orange {
+  background-color: var(--color-su-washed-orange);
+}
+.bg-su-light-blue {
+  background-color: var(--color-su-light-blue);
+}
+.bg-su-dark-blue {
+  background-color: var(--color-su-dark-blue);
+}
+html {
+  background-color: var(--color-su-washed-orange);
+}
 
 @media (max-width: 60em) {
   .br--top-nl {
@@ -121,7 +203,7 @@ html { background-color: var(--color-su-washed-orange);}
     border-bottom-right-radius: 0;
   }
 }
-.objfit-cover  {
+.objfit-cover {
   object-fit: cover;
 }
 .ml-2 {
