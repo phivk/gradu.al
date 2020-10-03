1e87608ecbe63584c097adce5a7d9b0d6027c5d3
Philo van Kemenade <philovankemenade@gmail.com>
2020-10-03 14:54:41 +0200

    componentise SessionCard

---
+++ components/SessionCard.vue
@@ -0,0 +1,81 @@
+<template>
+  <article class="tl bg-white-80 br3 ba-hover-dark-blue">
+    <div class="flex flex-column flex-row-l">
+      <div class="w-100 w-40-l">
+        <a
+          :href="link"
+          target="_blank"
+          class="db no-underline black w-100 h-100"
+        >
+          <img
+            :src="imageSrc"
+            :alt="title"
+            class="db w-100 h-100 br3  br--top-nl br--left-l objfit-cover"
+          />
+        </a>
+      </div>
+      <div class="w-100 w-60-l pa3">
+        <div class="flex items-center justify-between flex-wrap">
+          <span
+            class="f7 bg-light-green dark-green ph2 pv1 mv1 dib br-pill"
+            >{{ type }}</span
+          >
+          <span class="o-70">{{ date }}</span>
+        </div>
+        <h3 class="mt1 lh-title">
+          {{ title }}
+        </h3>
+        <div class="lh-copy measure mt2">
+          <h3 class="f5">learned by</h3>
+          <ul class="flex list pa0 ml2">
+            <li v-for="learner in learners" class="ml-2">
+              <img
+                class="br-100 w2 dib ba b--white bw2"
+                :src="learner.profilePic"
+                :alt="`profile picture of ${learner.userName}`"
+                :title="learner.userName"
+              />
+            </li>
+          </ul>
+          <h3 class="f5">shared by</h3>
+          <ul class="flex list pa0 ml2">
+            <li v-for="sharer in sharers" class="ml-2">
+              <img
+                class="br-100 w2 dib ba b--white bw2"
+                :src="sharer.profilePic"
+                :alt="`profile picture of ${sharer.userName}`"
+                :title="sharer.userName"
+              />
+            </li>
+          </ul>
+          <h3 class="f5">resources</h3>
+          <ul class="list pa0">
+            <li
+              class="di"
+              v-for="(resource, index) in resources"
+            >
+              <span v-if="index !== 0">, </span>
+              <a class="dark-green f6" :href="resource.href">{{
+                resource.text
+              }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+<script>
+export default {
+  props: {
+    title: { type: String, default: "Title" },
+    type: { type: String, default: "type" },
+    date: { type: String, default: "date" },
+    imageSrc: { type: String },
+    link: { type: String },
+    learners: { type: Array, default: [] },
+    sharers: { type: Array, default: [] },
+    resources: { type: Array, default: [] },
+  },
+};
+</script>
--- pages/storytellersunited/index.vue
+++ pages/storytellersunited/index.vue
@@ -44,71 +44,16 @@
         <h2>Things we've learned in previous sessions</h2>
         <ul class="list pa0 ma0 flex flex-wrap">
           <li v-for="session in sessions" class="center w-100 w-50-m mw8 pa3">
-            <article class="tl bg-white-80 br3 ba-hover-dark-blue">
-              <div class="flex flex-column flex-row-l">
-                <div class="w-100 w-40-l">
-                  <a
-                    :href="session.link"
-                    target="_blank"
-                    class="db no-underline black w-100 h-100"
-                  >
-                    <img
-                      :src="session.imageSrc"
-                      :alt="session.title"
-                      class="db w-100 h-100 br3  br--top-nl br--left-l objfit-cover"
-                    />
-                  </a>
-                </div>
-                <div class="w-100 w-60-l pa3">
-                  <div class="flex items-center justify-between flex-wrap">
-                    <span
-                      class="f7 bg-light-green dark-green ph2 pv1 mv1 dib br-pill"
-                      >{{ session.type }}</span
-                    >
-                    <span class="o-70">{{ session.date }}</span>
-                  </div>
-                  <h3 class="mt1 lh-title">
-                    {{ session.title }}
-                  </h3>
-                  <div class="lh-copy measure mt2">
-                    <h3 class="f5">learned by</h3>
-                    <ul class="flex list pa0 ml2">
-                      <li v-for="learner in session.learners" class="ml-2">
-                        <img
-                          class="br-100 w2 dib ba b--white bw2"
-                          :src="learner.profilePic"
-                          :alt="`profile picture of ${learner.userName}`"
-                          :title="learner.userName"
-                        />
-                      </li>
-                    </ul>
-                    <h3 class="f5">shared by</h3>
-                    <ul class="flex list pa0 ml2">
-                      <li v-for="sharer in session.sharers" class="ml-2">
-                        <img
-                          class="br-100 w2 dib ba b--white bw2"
-                          :src="sharer.profilePic"
-                          :alt="`profile picture of ${sharer.userName}`"
-                          :title="sharer.userName"
-                        />
-                      </li>
-                    </ul>
-                    <h3 class="f5">resources</h3>
-                    <ul class="list pa0">
-                      <li
-                        class="di"
-                        v-for="(resource, index) in session.resources"
-                      >
-                        <span v-if="index !== 0">, </span>
-                        <a class="dark-green f6" :href="resource.href">{{
-                          resource.text
-                        }}</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </article>
+            <SessionCard
+              :title="session.title"
+              :type="session.type"
+              :date="session.date"
+              :imageSrc="session.imageSrc"
+              :link="session.link"
+              :learners="session.learners"
+              :sharers="session.sharers"
+              :resources="session.resources"
+            />
           </li>
         </ul>
       </section>
@@ -118,6 +63,7 @@
 
 <script>
 import Logo from "~/components/Logo.vue";
+import SessionCard from "~/components/SessionCard.vue";
 export default {
   head() {
     return {
@@ -126,6 +72,7 @@ export default {
   },
   components: {
     Logo,
+    SessionCard,
   },
   data: function() {
     return {
