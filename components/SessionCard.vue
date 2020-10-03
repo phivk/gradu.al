01c602cc02d2ce47a37df048c5f350f5147f85dd
Philo van Kemenade <philovankemenade@gmail.com>
2020-10-03 16:49:41 +0200

    improve SessionCard styling

--- components/SessionCard.vue
+++ components/SessionCard.vue
@@ -16,46 +16,48 @@
       </div>
       <div class="w-100 w-60-l pa3">
         <div class="flex items-center justify-between flex-wrap">
-          <span
-            class="f7 bg-light-green dark-green ph2 pv1 mv1 dib br-pill"
-            >{{ type }}</span
-          >
-          <span class="o-70">{{ date }}</span>
+          <span class="f7 bg-light-green dark-green ph2 pv1 mv1 dib br-pill">{{
+            type
+          }}</span>
+          <span class="f5 o-70">{{ date }}</span>
         </div>
-        <h3 class="mt1 lh-title">
+        <h3 class="f4 f3-l mt2 lh-title">
           {{ title }}
         </h3>
-        <div class="lh-copy measure mt2">
-          <h3 class="f5">learned by</h3>
-          <ul class="flex list pa0 ml2">
-            <li v-for="learner in learners" class="ml-2">
-              <img
-                class="br-100 w2 dib ba b--white bw2"
-                :src="learner.profilePic"
-                :alt="`profile picture of ${learner.userName}`"
-                :title="learner.userName"
-              />
-            </li>
-          </ul>
-          <h3 class="f5">shared by</h3>
-          <ul class="flex list pa0 ml2">
-            <li v-for="sharer in sharers" class="ml-2">
-              <img
-                class="br-100 w2 dib ba b--white bw2"
-                :src="sharer.profilePic"
-                :alt="`profile picture of ${sharer.userName}`"
-                :title="sharer.userName"
-              />
-            </li>
-          </ul>
-          <h3 class="f5">resources</h3>
+        <div class="mt2 flex flex-wrap">
+          <div class="mr3">
+            <h3 class="f5 fw4 o-70">learned by</h3>
+            <ul class="flex list pa0 ml2">
+              <li v-for="learner in learners" class="ml-2">
+                <img
+                  class="br-100 w2 dib ba b--white bw2"
+                  :src="learner.profilePic"
+                  :alt="`profile picture of ${learner.userName}`"
+                  :title="learner.userName"
+                />
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="f5 fw4 o-70">shared by</h3>
+            <ul class="flex list pa0 ml2">
+              <li v-for="sharer in sharers" class="ml-2">
+                <img
+                  class="br-100 w2 dib ba b--white bw2"
+                  :src="sharer.profilePic"
+                  :alt="`profile picture of ${sharer.userName}`"
+                  :title="sharer.userName"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="mt2">
+          <h3 class="f5 fw4 o-70">resources</h3>
           <ul class="list pa0">
-            <li
-              class="di"
-              v-for="(resource, index) in resources"
-            >
+            <li class="di" v-for="(resource, index) in resources">
               <span v-if="index !== 0">, </span>
-              <a class="dark-green f6" :href="resource.href">{{
+              <a class="dark-green" :href="resource.href">{{
                 resource.text
               }}</a>
             </li>
