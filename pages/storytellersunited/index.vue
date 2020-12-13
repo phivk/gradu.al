cbb458b987b6f9003381c7d1adb7923f516d4d9b
Philo van Kemenade <philovankemenade@gmail.com>
2020-12-13 13:17:28 +0100

    add margin bottom to graph to fix jump on mobile

--- pages/storytellersunited/index.vue
+++ pages/storytellersunited/index.vue
@@ -83,7 +83,7 @@
         <p class="mb3 f4 fw3 measure center">
           Here's an interactive map of skills that SU members would like to <span class="b orange">↗︎learn</span> or <span class="b purple">↗︎share</span>. Click any <span class="b dark-green">●skill</span> or <span class="b dark-blue">●member</span> to explore the connections!
         </p>
-        <div class="graph-container" 
+        <div class="graph-container mb4" 
             @click="graphClicked = true"
             @mouseleave="graphClicked = false"
           >
