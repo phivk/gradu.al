c3b2d8f092e9594d10b57ff5230c2a57521788ca
Philo van Kemenade <philovankemenade@gmail.com>
2020-12-18 19:55:25 +0100

    tweak calendar link

--- pages/storytellersunited/index.vue
+++ pages/storytellersunited/index.vue
@@ -41,7 +41,7 @@
       <section v-if="sessionsUpcoming.length" class="mv5">
         <h2 class="mb3">Upcoming Sessions</h2>
         <p class="mb3 measure-wide center lh-copy">
-          If you'd like to stay up to date about upcoming sessions, you can <a href="https://storytellers.link/calendar/" class="su-dark-orange" target="_blank">subscribe to the SU community calendar</a>.
+          If you'd like to stay up to date about upcoming sessions, you can subscribe to the <a href="https://storytellers.link/calendar/" class="su-dark-orange" target="_blank">SU community calendar</a>.
         </p>
         <ul class="list pa0 ma0 flex flex-wrap">
           <li
