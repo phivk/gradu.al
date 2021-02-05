f638ceeab80a86047414d95eab80f333fc6ed4b8
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-02-05 14:47:32 +0000

    Adds CORS to the API route.

--- api/slack-token.js
+++ api/slack-token.js
@@ -1,7 +1,28 @@
 const FormData = require('form-data')
 const fetch = require("node-fetch")
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: ['GET', 'HEAD', 'POST'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 
 module.exports = async (req, res) => {
+  await runMiddleware(req, res, cors)
   try {
     const form = new FormData()
     form.append("client_id", process.env.SLACK_CLIENT_ID);
