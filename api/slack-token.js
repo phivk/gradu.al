650776f62ca67fd3527436026db901a8cdfd6216
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-02-12 19:04:56 +0000

    Testing again.

--- api/slack-token.js
+++ api/slack-token.js
@@ -1,41 +1,39 @@
 const FormData = require('form-data')
 const fetch = require("node-fetch")
-// // import Cors from 'cors'
+import Cors from 'cors'
 
-// // const cors = Cors({
-// //   methods: ['GET', 'HEAD', 'POST'],
-// // })
+const cors = Cors({
+  methods: ['GET', 'HEAD', 'POST'],
+})
 
-// // // Helper method to wait for a middleware to execute before continuing
-// // // And to throw an error when an error happens in a middleware
-// // function runMiddleware(req, res, fn) {
-// //   return new Promise((resolve, reject) => {
-// //     fn(req, res, (result) => {
-// //       if (result instanceof Error) {
-// //         return reject(result)
-// //       }
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
 
-// //       return resolve(result)
-// //     })
-// //   })
-// // }
+      return resolve(result)
+    })
+  })
+}
 
 module.exports = async (req, res) => {
-  const variables = process.env
-  res.send(req.body, JSON.stringify(variables))
-  // await runMiddleware(req, res, cors)
-  // try {
-  //   const form = new FormData()
-  //   form.append("client_id", process.env.SLACK_CLIENT_ID);
-  //   form.append("client_secret", process.env.SLACK_CLIENT_SECRET);
-  //   form.append("redirect_uri", process.env.SLACK_REDIRECT_URI);
-  //   form.append("code", req.body.code)
+  await runMiddleware(req, res, cors)
+  try {
+    const form = new FormData()
+    form.append("client_id", process.env.SLACK_CLIENT_ID);
+    form.append("client_secret", process.env.SLACK_CLIENT_SECRET);
+    form.append("redirect_uri", process.env.SLACK_REDIRECT_URI);
+    form.append("code", req.body.code)
 
-  //   const response = await fetch("https://slack.com/api/oauth.v2.access", { method: 'POST', body: form })
-  //     .then((res) => res.json())
+    const response = await fetch("https://slack.com/api/oauth.v2.access", { method: 'POST', body: form })
+      .then((res) => res.json())
 
-  //   res.send(response)
-  // } catch (error) {
-  //   res.status(400).send(error)
-  // }
+    res.send(response)
+  } catch (error) {
+    res.status(400).send(error)
+  }
 }
