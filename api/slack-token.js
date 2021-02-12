4dc3d854f2208f9e1895acc40eded12d08517b10
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-02-12 18:41:54 +0000

    Simplifying function.

--- api/slack-token.js
+++ api/slack-token.js
@@ -23,19 +23,19 @@ function runMiddleware(req, res, fn) {
 
 module.exports = async (req, res) => {
   res.send(req.body, process.env.SLACK_CLIENT_ID, process.env.SLACK_CLIENT_SECRET, process.env.SLACK_REDIRECT_URI)
-  await runMiddleware(req, res, cors)
-  try {
-    const form = new FormData()
-    form.append("client_id", process.env.SLACK_CLIENT_ID);
-    form.append("client_secret", process.env.SLACK_CLIENT_SECRET);
-    form.append("redirect_uri", process.env.SLACK_REDIRECT_URI);
-    form.append("code", req.body.code)
+  // await runMiddleware(req, res, cors)
+  // try {
+  //   const form = new FormData()
+  //   form.append("client_id", process.env.SLACK_CLIENT_ID);
+  //   form.append("client_secret", process.env.SLACK_CLIENT_SECRET);
+  //   form.append("redirect_uri", process.env.SLACK_REDIRECT_URI);
+  //   form.append("code", req.body.code)
 
-    const response = await fetch("https://slack.com/api/oauth.v2.access", { method: 'POST', body: form })
-      .then((res) => res.json())
+  //   const response = await fetch("https://slack.com/api/oauth.v2.access", { method: 'POST', body: form })
+  //     .then((res) => res.json())
 
-    res.send(response)
-  } catch (error) {
-    res.status(400).send(error)
-  }
+  //   res.send(response)
+  // } catch (error) {
+  //   res.status(400).send(error)
+  // }
 }
