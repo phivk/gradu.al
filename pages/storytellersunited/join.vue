dedd08f16720d0c692c623a5c9625cdc4417d01f
Philo van Kemenade <philovankemenade@gmail.com>
2021-02-22 22:33:52 +0100

    refactor: componentise TypeFormEmbed

--- pages/storytellersunited/join.vue
+++ pages/storytellersunited/join.vue
@@ -1,43 +1,25 @@
 <template>
-  <div>
-    <iframe
-      id="typeform-full"
-      width="100%"
-      height="100%"
-      frameborder="0"
-      allow="camera; microphone; autoplay; encrypted-media;"
-      src="https://form.typeform.com/to/nqEQHQ"
-    ></iframe>
-    <script
-      type="text/javascript"
-      src="https://embed.typeform.com/embed.js"
-    ></script>
-  </div>
+  <TypeFormEmbed
+    :typeFormSrc="typeFormSrc"
+  />
 </template>
 
 <script>
+import TypeFormEmbed from "~/components/TypeFormEmbed.vue";
 export default {
   head() {
     return {
-      title: "Storytellers United - Here to learn"
-    }
-  }
+      title: "Storytellers United - Here to learn",
+    };
+  },
+  components: {
+    TypeFormEmbed,
+  },
+  data() {
+    return {
+      typeFormSrc: "https://form.typeform.com/to/nqEQHQ",
+    };
+  },
 };
 </script>
 
-<style scoped>
-html {
-  margin: 0;
-  height: 100%;
-  overflow: hidden;
-  background-color: #fff;
-}
-iframe {
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  border: 0;
-}
-</style>
