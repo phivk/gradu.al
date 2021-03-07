5b2cc4237c92c2222c17d69f5d07ecacca35d65d
Philo van Kemenade <philovankemenade@gmail.com>
2021-03-07 18:30:51 +0100

    add mozfest community spreadheetId

--- communities.js
+++ communities.js
@@ -1,10 +1,10 @@
 module.exports = [
-  //   {
-  //   "name": "storytellersunited",
-  //   "spreadsheetId": "1rL2WrQGe32t4vNkfku-_fbJBKwYUwRUV_ERwxdlkgO8"
-  // },
   {
     "name": "demo",
     "spreadsheetId": "16eV2VLLy1s0JYgL5PWXmw6yxGTVcHt1bnct2cSflx8A"
+  },
+  {
+    "name": "mozfest",
+    "spreadsheetId": "1V0VW1_6w56V4vgLKfxx1w15MKPdOM0JtAkpvHaEXyXA"
   },
 ]
