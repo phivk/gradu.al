995d1f872f7ea18a00b40c60728eb9bc7297cb29
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-03-07 15:43:52 +0000

    Update: Changes the community lable.

--- communities.js
+++ communities.js
@@ -4,7 +4,7 @@ module.exports = [
   //   "spreadsheetId": "1rL2WrQGe32t4vNkfku-_fbJBKwYUwRUV_ERwxdlkgO8"
   // },
   {
-    "name": "mozfest",
+    "name": "demo",
     "spreadsheetId": "16eV2VLLy1s0JYgL5PWXmw6yxGTVcHt1bnct2cSflx8A"
   },
 ]
