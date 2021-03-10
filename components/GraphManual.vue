672f338f1cd4191749eb6478b72f30e461e9d27a
Philo van Kemenade <philovankemenade@gmail.com>
2021-03-10 22:09:36 +0100

    style: fix missing semicolon and prettier format

--- components/GraphManual.vue
+++ components/GraphManual.vue
@@ -2,17 +2,37 @@
   <div>
     <svg class="h0">
       <defs>
-        <marker id="m-end" markerWidth="10" markerHeight="10" refX="9" refY="3" orient="auto" markerUnits="strokeWidth" >
+        <marker
+          id="m-end"
+          markerWidth="10"
+          markerHeight="10"
+          refX="9"
+          refY="3"
+          orient="auto"
+          markerUnits="strokeWidth"
+        >
           <path d="M0,0 L0,6 L9,3 z"></path>
         </marker>
-        <marker id="m-start" markerWidth="6" markerHeight="6" refX="-4" refY="3" orient="auto" markerUnits="strokeWidth" >
+        <marker
+          id="m-start"
+          markerWidth="6"
+          markerHeight="6"
+          refX="-4"
+          refY="3"
+          orient="auto"
+          markerUnits="strokeWidth"
+        >
           <rect width="3" height="6"></rect>
         </marker>
       </defs>
     </svg>
     <div class="relative">
       <d3-network
-        ref='net' :net-nodes="nodes" :net-links="edges" :options="options" :link-cb="lcb"
+        ref="net"
+        :net-nodes="nodes"
+        :net-links="edges"
+        :options="options"
+        :link-cb="lcb"
       />
       <div class="absolute bottom-0 left-0 mw6 ma3 net-menu br3">
         <h2 class="pt3">Change the force between the nodes to explore more</h2>
@@ -28,16 +48,16 @@
 </template>
 <style src="vue-d3-network/dist/vue-d3-network.css"></style>
 <script>
-import D3Network from 'vue-d3-network';
-export default({
+import D3Network from "vue-d3-network";
+export default {
   components: {
     D3Network,
   },
   props: {
     nodes: Array,
-    edges: Array
+    edges: Array,
   },
-  data () {
+  data() {
     return {
       nodeSize: 15,
       canvas: false,
@@ -47,44 +67,44 @@ export default({
       startY: 0,
       lastTouch: null,
       force: 2500,
-      offsetY: 0
-    }
+      offsetY: 0,
+    };
   },
-  computed:{
-    options(){
-      return{
+  computed: {
+    options() {
+      return {
         force: this.force,
         nodeSize: this.nodeSize,
         nodeLabels: true,
-        linkLabels:true,
+        linkLabels: true,
         canvas: this.canvas,
         resizeListener: true,
         size: {
-          h: 1000
+          h: 1000,
         },
         offset: {
           x: this.offsetX,
-          y: this.offsetY
-        }
-      }
-    }
+          y: this.offsetY,
+        },
+      };
+    },
   },
-  methods:{
-    lcb (link) {
-      link._svgAttrs = { 'marker-end': 'url(#m-end)' }
-      return link
-    }
+  methods: {
+    lcb(link) {
+      link._svgAttrs = { "marker-end": "url(#m-end)" };
+      return link;
+    },
   },
-})
+};
 </script>
-<style >
+<style>
 .net {
   background-color: white;
-  border-radius: 8px
+  border-radius: 8px;
 }
 
 .net-menu {
-  background: #96CCFF;
+  background: #96ccff;
   padding: 8px;
 }
 
@@ -96,13 +116,13 @@ ul.menu {
   text-align: left;
 }
 
-ul.menu li{
+ul.menu li {
   margin-top: 1em;
   position: relative;
 }
 
 circle.Skill.node {
-  fill:#4ba45b;
+  fill: #4ba45b;
 }
 
 circle.Member.node {
@@ -112,32 +132,33 @@ circle.Member.node {
 .net {
   height: 100%;
   margin: 0;
- }
+}
 
-.node{
+.node {
   stroke: alpha(red, 0.7);
   stroke-width: 3px;
   transition: fill 0.5s ease;
   fill: white;
 }
 
-path.learner{
+path.learner {
   stroke: black;
 }
 
-path.sharer{
+path.sharer {
   stroke: blue;
 }
 
-.node, .link {
+.node,
+.link {
   stroke-linecap: round;
 }
 
-.curve{
+.curve {
   fill: none;
 }
 
-.node-label{
+.node-label {
   /* fill: black; */
   font-size: 1.1rem;
 }
@@ -150,7 +171,8 @@ path.sharer{
   fill: green;
 }
 
-#m-end path, #m-start{
+#m-end path,
+#m-start {
   fill: rgba(18, 120, 98, 0.8);
 }
 
