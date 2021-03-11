c0c6c7e238fcea7cf16ad68e532cc66fde1af860
Philo van Kemenade <philovankemenade@gmail.com>
2021-03-12 00:10:59 +0100

    styling: add slight border and align text left on InfoBar

--- components/InfoBar.vue
+++ components/InfoBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <p class="center pa3 bg-color-accent-washed br2 flex items-center mw6">
-    <span class="mr2">
+  <p class="center pa3 bg-color-accent-washed ba b--black-10 br2 flex items-center mw6 tl">
+    <span class="mr2 f3">
       <slot name="icon"></slot>
     </span>
     <span>
