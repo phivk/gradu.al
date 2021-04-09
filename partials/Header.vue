4ea068044b6290162de2c4e0eaf9376eaba5678d
Philo van Kemenade <philovankemenade@gmail.com>
2021-04-10 00:35:20 +0200

    refactor: define links in data

--- partials/Header.vue
+++ partials/Header.vue
@@ -6,17 +6,8 @@
         <!-- Site branding -->
         <div class="flex-shrink-0 mr-4">
           <!-- Logo -->
-          <router-link to="/" class="block" aria-label="Cruip">
-            <svg class="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-              <defs>
-                <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="header-logo">
-                  <stop stop-color="#4FD1C5" offset="0%" />
-                  <stop stop-color="#81E6D9" offset="25.871%" />
-                  <stop stop-color="#338CF5" offset="100%" />
-                </radialGradient>
-              </defs>
-              <rect width="32" height="32" rx="16" fill="url(#header-logo)" fill-rule="nonzero" />
-            </svg>
+          <router-link to="/" class="block" aria-label="Gradual">
+            <logo class="w-12" is-black/>
           </router-link>
         </div>
 
@@ -25,40 +16,24 @@
 
           <!-- Desktop menu links -->
           <ul class="flex flex-grow justify-end flex-wrap items-center">
-            <li>
-              <router-link to="/pricing" class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">Pricing</router-link>
+            <li v-for="link in links">
+              <router-link :to="link.path" class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">{{link.text}}</router-link>
             </li>
-            <li>
-              <router-link to="/about" class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">About us</router-link>
-            </li>
-            <li>
-              <router-link to="/tutorials" class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">Tutorials</router-link>
-            </li>
-            <li>
-              <router-link to="/blog" class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out">Blog</router-link>
-            </li>            
-            <!-- 1st level: hover -->
-            <Dropdown title="Resources">
-              <li>
-                <router-link to="/documentation" class="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight">Documentation</router-link>
-              </li>
-              <li>
-                <router-link to="/support" class="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight">Support center</router-link>
-              </li>
-              <li>
-                <router-link to="/404" class="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight">404</router-link>
+            <Dropdown v-if="dropdownLinks.length" title="Use Cases">
+              <li v-for="ddLink in dropdownLinks">
+                <router-link :to="ddLink.path" class="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight">{{ddLink.text}}</router-link>
               </li>
             </Dropdown>
           </ul>
 
           <!-- Desktop sign in links -->
           <ul class="flex flex-grow justify-end flex-wrap items-center">
-            <li>
+            <!-- <li>
               <router-link to="/signin" class="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</router-link>
-            </li>
+            </li> -->
             <li>
-              <router-link to="/signup" class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                <span>Sign up</span>
+              <router-link to="/get-demo" class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <span>Get a Demo</span>
                 <svg class="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                   <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill-rule="nonzero" />
                 </svg>
@@ -97,38 +72,23 @@
               class="absolute top-full h-screen pb-16 z-20 left-0 w-full overflow-scroll bg-white transition-all duration-300 ease-in-out"
             >
               <ul class="px-5 py-2">
-                <li>
-                  <router-link to="/pricing" class="flex text-gray-600 hover:text-gray-900 py-2">Pricing</router-link>
-                </li>
-                <li>
-                  <router-link to="/about" class="flex text-gray-600 hover:text-gray-900 py-2">About us</router-link>
-                </li>
-                <li>
-                  <router-link to="/tutorials" class="flex text-gray-600 hover:text-gray-900 py-2">Tutorials</router-link>
-                </li>
-                <li>
-                  <router-link to="/blog" class="flex text-gray-600 hover:text-gray-900 py-2">Blog</router-link>
-                </li>                
-                <li class="py-2 my-2 border-t border-b border-gray-200">
-                  <span class="flex text-gray-600 hover:text-gray-900 py-2">Resources</span>
+                <li v-for="link in links">
+                  <router-link :to="link.path" class="flex text-gray-600 hover:text-gray-900 py-2">{{link.text}}</router-link>
+                </li>          
+                <li v-if="dropdownLinks.length" class="py-2 my-2 border-t border-b border-gray-200">
+                  <span class="flex text-gray-600 hover:text-gray-900 py-2">Use Cases</span>
                   <ul class="pl-4">
-                    <li>
-                      <router-link to="/documentation" class="text-sm flex font-medium text-gray-600 hover:text-gray-900 py-2">Documentation</router-link>
-                    </li>
-                    <li>
-                      <router-link to="/support" class="text-sm flex font-medium text-gray-600 hover:text-gray-900 py-2">Support center</router-link>
-                    </li>
-                    <li>
-                      <router-link to="/404" class="text-sm flex font-medium text-gray-600 hover:text-gray-900 py-2">404</router-link>
+                    <li v-for="ddLink in dropdownLinks">
+                      <router-link :to="ddLink.path" class="text-sm flex font-medium text-gray-600 hover:text-gray-900 py-2">{{ddLink.text}}</router-link>
                     </li>
                   </ul>
                 </li>
-                <li>
+                <!-- <li>
                   <router-link to="/signin" class="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center">Sign in</router-link>
-                </li>
+                </li> -->
                 <li>
-                  <router-link to="/signup" class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2">
-                    <span>Sign up</span>
+                  <router-link to="/get-demo" class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2">
+                    <span>Get a Demo</span>
                     <svg class="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill="#999" fill-rule="nonzero" />
                     </svg>                
@@ -147,16 +107,30 @@
 
 <script>
 import Dropdown from './../utils/Dropdown.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   name: 'Header',
   components: {
-    Dropdown
+    Dropdown, 
+    Logo, 
   },
   data: function () {
     return {
       mobileNavOpen: false,
-      top: true
+      top: true,
+      links: [
+        {
+          path: '/mozfest',
+          text: 'MozFest',
+        },
+        {
+          path: '/storytellersunited',
+          text: 'Storytellers United',
+        },
+      ],
+      dropdownLinks: [
+      ]
     }
   },
   methods: {
