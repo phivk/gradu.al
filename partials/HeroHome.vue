c3aefb0ee6769373eb00cea2dedec28e7c740ec6
Philo van Kemenade <philovankemenade@gmail.com>
2021-04-14 13:07:19 +0200

    decrease hero title font size on narrow screens

--- partials/HeroHome.vue
+++ partials/HeroHome.vue
@@ -25,7 +25,7 @@
 
         <!-- Section header -->
         <div class="text-center pb-12 md:pb-16">
-          <h1 class="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
+          <h1 class="text-4xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
             You <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-400">learn</span> more<br>
             when you <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-400">share</span>
           </h1>
