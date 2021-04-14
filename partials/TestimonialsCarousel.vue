29dbea6d2c4de3f61d86edb2a8fcd9256e1c35c2
Philo van Kemenade <philovankemenade@gmail.com>
2021-04-14 12:45:48 +0200

    add role maria

--- partials/TestimonialsCarousel.vue
+++ partials/TestimonialsCarousel.vue
@@ -88,7 +88,7 @@ export default {
             '“ Using Gradual was a delight! I got quick feedback on a workshop in a setting that was low risk, but high value. After our session, I put work I had prepared into an article which has ended up being really personally useful for me. “',
           name: 'Maria Bridge',
           role: 'Stanford MBA',
-          // team: '@Shopify',
+          team: 'Non-profit COO',
           link: 'https://mariabridge.medium.com/'
         },   
         {
