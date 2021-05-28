9f8a4a0d01ebc3040a66d0611bef55683b93e090
Philo van Kemenade <philovankemenade@gmail.com>
2021-05-28 15:24:52 +0200

    tidy code style and deprecated classes

--- components/NavigationTabs.vue
+++ components/NavigationTabs.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="tabs" v-bind:style="{ width: width, height: height }">
+  <div :style="{ width: width, height: height }">
     <nav class="flex flex-row">
       <button
         class="text-gray-600 p-4 block hover:text-green-500 focus:outline-none border-green-500"
-        v-bind:class="{ tabActive: tabIndex == index }"
+        :class="{ tabActive: tabIndex == index }"
         v-for="(name, index) in names"
         @click="tabIndex = index"
       >
         {{ name }}
       </button>
     </nav>
-    <div class="tab-content p-4">
+    <div class="p-4">
       <div
         v-for="(name, index) in names"
         :key="`tab${index}`"
         v-if="tabIndex == index"
       >
-        <slot :name="`tab${index}`"></slot>
+        <slot :name="`tab${index}`" />
       </div>
     </div>
   </div>
@@ -48,6 +48,6 @@ export default {
 
 <style scoped>
 .tabActive {
-    @apply text-green-500 border-b-2 font-medium border-green-500
+  @apply text-green-500 border-b-2 font-medium border-green-500;
 }
 </style>
