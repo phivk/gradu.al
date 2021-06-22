c5893fe6571accf286cdd64f7a9dd134a8b83198
Philo van Kemenade <philovankemenade@gmail.com>
2021-06-22 23:51:44 +0200

    update demo page to match /mozfest

--- pages/demo/index.vue
+++ pages/demo/index.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
+    <social-head
+     title="Demo - Here to learn"
+     description=""
+    />
     <div class="text-center">
       <section class="mb-16">
         <div class="mv4">
           <a href="https://www.mozillafestival.org/" target="_blank">
             <img
-              class="w4 w5-ns"
+              class="dib w4 w5-ns"
               src="https://assets.mofoprod.net/static/_images/mozfest/logo.364cf5df72b2.svg"
-              alt="MozFest Logo"
+              alt="Demo Logo"
             />
           </a>
         </div>
-        <h1 class="f2 f1-ns">What would you like to learn or share?</h1>
+        <h1 class="text-4xl md:text-5xl font-bold">What would you like to learn or share?</h1>
         <nuxt-link
           append
           to="join"
-          class="dib mb-4 sm:mb-8 f3 f2-ns br3 ph3 pv2 no-underline grow shadow-hover white bg-color-accent"
+          class="dib mb-8 text-3xl br3 ph3 pv2 no-underline grow shadow-hover white bg-color-accent"
         >
           Let us know!
         </nuxt-link>
@@ -30,22 +34,75 @@
       </section>
       <section class="mb-16">
         <div class="center">
-          <h2 class="measure center mv2">
+          <h2 class="text-2xl md:text-3xl font-bold measure center mv2">
             Hi 👋 <br />
             Welcome to Demo on Gradual!
           </h2>
-          <div class="measure center">
-            <p class="mb-4 f4 lh-copy">
+          <div class="center">
+            <p class="center measure mb-4 f4 lh-copy">
               This is a space to express things you’d like to learn and share
-              with others in the community. Keep an eye on the
+              with others in the community. Join the conversation on the
               <a
                 href="https://app.slack.com/client/T170JCUN6/C01PXSJ9AH0"
                 target="_blank"
                 class="color-accent underline hover:no-underline"
-                >#skillsharing channel</a
+                >#skillsharing</a
               >
-              for updates.
+              channel!
             </p>
+            <ol class="list p-0 mb-4 flex flex-wrap justify-center">
+              <li class="p-4 w-100 w-third-ns mw5-5">
+                <ProcessCard number="1" title="Map">
+                  Let us know what you'd like to learn or share. Anything is
+                  welcome!
+                </ProcessCard>
+              </li>
+              <li class="p-4 w-100 w-third-ns mw5-5">
+                <ProcessCard number="2" title="Match">
+                  See how Demo is connected by skills! We help you plan a
+                  session in the format you choose.
+                </ProcessCard>
+              </li>
+              <li class="p-4 w-100 w-third-ns mw5-5">
+                <ProcessCard number="3" title="Meet">
+                  Host or join a session to learn with other Demo
+                  participants based on your interests!
+                </ProcessCard>
+              </li>
+            </ol>
+            <p class="measure center mb-8 lh-copy">
+              <strong>Sessions take any format</strong> that best fits the
+              facilitator and the skill they are sharing, for example
+              <TagPill
+                v-for="sessionType in sessionTypes"
+                :key="sessionType"
+                class="m-1"
+              >
+                {{ sessionType }}
+              </TagPill>
+            </p>
+            <InfoBar>
+              <template v-slot:left>
+                🤔
+              </template>
+              <template v-slot:middle>
+                Need more help?
+                <a
+                  class="color-accent underline hover:no-underline"
+                  href="https://www.loom.com/share/406bfe57b075452a8efadbff954191ad"
+                  target="_blank"
+                  >Watch a walk-through video</a
+                >.
+              </template>
+              <template v-slot:right>
+                <a
+                  class="color-accent no-underline"
+                  href="https://www.loom.com/share/406bfe57b075452a8efadbff954191ad"
+                  target="_blank"
+                  >↗︎</a
+                >
+              </template>
+            </InfoBar>
           </div>
         </div>
       </section>
@@ -72,11 +129,17 @@
 </template>
 
 <script>
-import SessionCardPreview from "~/components/SessionCardPreview.vue";
-import GraphCommonsEmbed from "~/components/GraphCommonsEmbed.vue";
 import SessionsSection from "~/components/SessionsSection.vue";
 import GraphSection from "~/components/GraphSection.vue";
-import GraphManual from "~/components/GraphManual.vue"
+import GraphManual from "~/components/GraphManual.vue";
+import GraphManualKey from "~/components/GraphManualKey.vue";
+import MostPopularSkillsSection from "~/components/MostPopularSkillsSection.vue";
+import CircleCharacter from "~/components/CircleCharacter.vue";
+import ProcessCard from "~/components/ProcessCard.vue";
+import TagPill from "~/components/TagPill.vue";
+import InfoBar from "~/components/InfoBar.vue";
+import SocialHead from "~/components/SocialHead.vue";
+import NavigationTabs from "~/components/NavigationTabs.vue";
 import { hasHappened, hasNotHappened } from "~/util/date";
 
 export default {
@@ -87,15 +150,33 @@ export default {
     };
   },
   components: {
-    SessionCardPreview,
-    GraphCommonsEmbed,
     SessionsSection,
     GraphSection,
-    GraphManual
+    GraphManual,
+    GraphManualKey,
+    MostPopularSkillsSection,
+    CircleCharacter,
+    ProcessCard,
+    TagPill,
+    InfoBar,
+    SocialHead,
+    NavigationTabs,
   },
   data() {
     return {
       graphCommonsSrc: undefined,
+      sessionTypes: [
+        "peer sharing",
+        "salon",
+        "conversation",
+        "workshop",
+        "lunch & learn",
+        "talk",
+        "one-on-one",
+        "presentation",
+        "brainstorm",
+        "AMA",
+      ],
     };
   },
   async asyncData({ $content }) {
@@ -130,3 +211,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+.mw5-5 {
+  max-width: 24rem;
+}
+</style>
