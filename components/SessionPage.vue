e322047432cc88156bde3059ed262df763a79c4e
Philo van Kemenade <philovankemenade@gmail.com>
2021-08-12 00:53:37 +0200

    add panopto recordings

--- components/SessionPage.vue
+++ components/SessionPage.vue
@@ -27,6 +27,9 @@
                 allowfullscreen
               ></iframe>
             </div>
+            <div class="video-wrapper" v-else-if="session.panoptoRecordingID">
+              <iframe :src="`https://mozilla.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=${session.panoptoRecordingID}&autoplay=false&offerviewer=false&showtitle=true&showbrand=false&captions=true&interactivity=none`" height="1080" width="1920" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+            </div>
             <img
               v-else
               class="db mb-4 sm:mb-8"
