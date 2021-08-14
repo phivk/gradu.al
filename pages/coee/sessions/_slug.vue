b529e17a52a00325e2db5c1b7f61a1d07264fbc5
Philo van Kemenade <philovankemenade@gmail.com>
2021-08-14 16:03:56 +0200

    show RedirectPage on session links

--- pages/coee/sessions/_slug.vue
+++ pages/coee/sessions/_slug.vue
@@ -1,80 +1,21 @@
 <template>
-  <SessionPage :session="session" :bgColor="bgColor" />
+  <redirect-page :newURL="newURL" />
 </template>
 
 <script>
-import TagPill from "~/components/TagPill.vue";
-import ProfilePicList from "~/components/ProfilePicList.vue";
-import SessionPage from "~/components/SessionPage.vue";
-
+import RedirectPage from "~/components/RedirectPage.vue";
 export default {
   layout: "coee",
-  middleware: ["password-protect"],
   head() {
     return {
       title: "CoEE - Here to learn",
     };
   },
-  components: { TagPill, ProfilePicList, SessionPage },
-  data() {
-    return {
-      bgColor: "#000",
-    };
-  },
-  async asyncData({ $content, params }) {
-    const session = await $content("coee/sessions", params.slug).fetch();
-    return { session };
-  },
+  components: { RedirectPage },
   computed: {
-    hasHappened() {
-      let now = new Date();
-      return new Date(this.session.date) < now;
-    },
-    youtubeRecordingResource() {
-      if (this.session.resources) {
-        return this.session.resources.find((r) => r.href.includes("youtu"));
-      } else {
-        return undefined;
-      }
-    },
-    youtubeRecordingID() {
-      if (this.youtubeRecordingResource) {
-        let parts = this.youtubeRecordingResource.href.split(/[/=]/);
-        return parts[parts.length - 1];
-      } else {
-        return undefined;
-      }
-    },
-    showICal() {
-      return this.session.icsFileSrc && !this.hasHappened;
-    },
-    dateFormatted() {
-      let dt = new Date(this.session.date);
-      return dt.toLocaleDateString("en-GB", {
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-      });
+    newURL() {
+      return `https://coee.gradu.al/sessions/${this.$route.params.slug}`
     },
   },
 };
 </script>
-<style>
-.nuxt-content p {
-  margin-bottom: 1rem;
-}
-</style>
-<style scoped>
-.video-wrapper {
-  position: relative;
-  padding-bottom: 56.25%; /* 16:9 */
-  height: 0;
-}
-.video-wrapper iframe {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-</style>
