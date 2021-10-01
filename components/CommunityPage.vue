0af90bfa4ebff8688e1867197c1d03aeb2aa50af
Philo van Kemenade <philovankemenade@gmail.com>
2021-10-01 11:25:00 +0200

    fix components for coee integration

--- components/CommunityPage.vue
+++ components/CommunityPage.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="text-center">
     <SocialHead :title="`${indexPage.communityName} - Here to learn`" />
+    <h2 v-if="indexPage.pageHeading" class="text-3xl md:text-4xl mb-6">
+      {{ indexPage.pageHeading }}
+    </h2>
+    <h3 v-if="indexPage.pageSubHeading" class="text-xl md:text-2xl font-normal">
+      {{ indexPage.pageSubHeading }}
+    </h3>
     <CTASection
       :logoSrc="indexPage.logoSrc"
       :communityName="indexPage.communityName"
