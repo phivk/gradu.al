1257d0389b7c7547224c3fef4e1209a59d95d70e
Philo van Kemenade <philovankemenade@gmail.com>
2021-10-05 23:23:05 +0200

    update community config and fetch data

--- communities.js
+++ communities.js
@@ -3,12 +3,16 @@ module.exports = [
   //   "name": "demo",
   //   "spreadsheetId": "16eV2VLLy1s0JYgL5PWXmw6yxGTVcHt1bnct2cSflx8A"
   // },
+  // {
+  //   name: "mozfest",
+  //   spreadsheetId: "1V0VW1_6w56V4vgLKfxx1w15MKPdOM0JtAkpvHaEXyXA"
+  // },
+  // {
+  //   name: "coee",
+  //   spreadsheetId: "1KGEbhYWfMMAZx23JvGRhS59v7WzjiyzORrPHNLkdqhw"
+  // },
   {
-    name: "mozfest",
-    spreadsheetId: "1V0VW1_6w56V4vgLKfxx1w15MKPdOM0JtAkpvHaEXyXA"
-  },
-  {
-    name: "coee",
-    spreadsheetId: "1KGEbhYWfMMAZx23JvGRhS59v7WzjiyzORrPHNLkdqhw"
+    name: "labsng",
+    spreadsheetId: "1a9Nfoa58oR1ar_8BoXeE5c3T8Tsmhm9ZIhhx7tCEvgw"
   }
 ];
