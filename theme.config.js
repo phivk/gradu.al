7ec5d6c4138015433ca524218e31afb104dc764d
Philo van Kemenade <philovankemenade@gmail.com>
2021-10-29 15:04:29 +0200

    rename theme washed colour version

--- theme.config.js
+++ theme.config.js
@@ -3,11 +3,11 @@ module.exports = {
   communityName: "Demo",
   colors: {
     primary: {
-      100: '#EFE5F8',
+      washed: '#EFE5F8',
       DEFAULT: '#6703BF',
     },
     secondary: {
-      100: '#F7E5F2',
+      washed: '#F7E5F2',
       DEFAULT: '#B7007E',
     },
   }
