120a164456873b1b0566b74b49895cdd6a9cb151
Philo van Kemenade <philovankemenade@gmail.com>
2022-01-26 21:15:24 +0100

    prettier format

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,34 @@
 <template>
   <div class="flex flex-col min-h-screen overflow-hidden">
-
     <!-- Site header -->
     <Header />
 
     <!-- Page content -->
     <main class="flex-grow">
-
       <!-- Page sections -->
       <HeroHome />
       <FeaturesProcess />
       <UseCases />
       <TestimonialsCarousel />
       <Cta />
-
     </main>
 
     <!-- Site footer -->
     <Footer />
-
   </div>
 </template>
 
 <script>
-import AOS from 'aos'
-import Sticky from 'sticky-js'
-import Header from './../partials/Header.vue'
-import HeroHome from './../partials/HeroHome.vue'
-import FeaturesProcess from './../partials/FeaturesProcess.vue'
-import TestimonialsCarousel from './../partials/TestimonialsCarousel.vue'
-import FeaturesBlocks from './../partials/FeaturesBlocks.vue'
-import UseCases from './../partials/UseCases.vue'
-import Cta from './../partials/Cta.vue'
-import Footer from './../partials/Footer.vue'
+import AOS from "aos";
+import Sticky from "sticky-js";
+import Header from "./../partials/Header.vue";
+import HeroHome from "./../partials/HeroHome.vue";
+import FeaturesProcess from "./../partials/FeaturesProcess.vue";
+import TestimonialsCarousel from "./../partials/TestimonialsCarousel.vue";
+import FeaturesBlocks from "./../partials/FeaturesBlocks.vue";
+import UseCases from "./../partials/UseCases.vue";
+import Cta from "./../partials/Cta.vue";
+import Footer from "./../partials/Footer.vue";
 
 export default {
   components: {
@@ -48,19 +44,19 @@ export default {
   mounted() {
     AOS.init({
       once: true,
-      disable: 'phone',
+      disable: "phone",
       duration: 700,
-      easing: 'ease-out-cubic',
-    })
+      easing: "ease-out-cubic",
+    });
     // eslint-disable-next-line no-unused-vars
-    const sticky = new Sticky('[data-sticky]');    
+    const sticky = new Sticky("[data-sticky]");
     // Route change
     if (this.$router) {
-      this.$watch('$route', () => {
+      this.$watch("$route", () => {
         // eslint-disable-next-line no-unused-vars
-        const sticky = new Sticky('[data-sticky]'); 
+        const sticky = new Sticky("[data-sticky]");
       });
-    }    
-  }
-}
+    }
+  },
+};
 </script>
