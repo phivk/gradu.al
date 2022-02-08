61b5cda4ac7c98754790b4e5f3e2af5a6ba735e0
Philo van Kemenade <philovankemenade@gmail.com>
2022-02-08 20:52:17 +0100

    improve component theming
    (cherry picked from commit c4825e2466eebb66a233bb14a7e25bea3d237682)

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -2,20 +2,20 @@
   <nav>
     <div class="p-4 ph4-m ph5-l mw9 mx-auto flex items-center justify-between">
       <NuxtLink to="/" class="flex items-center no-underline">
-        <logo class="w2-5" />
-        <span class="ml-2 text-white f3 font-bold dn di-ns">gradual</span>
+        <logo :isBlack="logoIsBlack" class="w2-5" />
+        <span class="ml-2 text-on-primary f3 font-bold dn di-ns">gradual</span>
       </NuxtLink>
       <AppLink
         v-if="showHowtoLink"
         to="/howto"
-        class="no-underline f4 fw4 text-white br-pill ph3 pv2 bg-animate hover-bg-white-20 cursor-pointer"
+        class="no-underline f4 fw4 text-on-primary br-pill ph3 pv2 bg-animate hover-bg-white-20 cursor-pointer"
       >
         how to
       </AppLink>
       <div v-if="showBackLink">
         <AppLink
           :to="backLinkTarget"
-          class="no-underline f4 fw4 text-white br-pill ph3 pv2 bg-animate hover-bg-white-20 cursor-pointer"
+          class="no-underline f4 fw4 text-on-primary br-pill ph3 pv2 bg-animate hover-bg-white-20 cursor-pointer"
         >
           ← {{ backLinkText }}
         </AppLink>
@@ -34,6 +34,7 @@ export default {
   props: {
     backLinkText: { type: String, default: "back" },
     backLinkTarget: { type: String, default: "/" },
+    logoIsBlack: { type: Boolean, default: false },
   },
   computed: {
     showBackLink() {
