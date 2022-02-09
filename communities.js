0035bf31aa9f3df891ff329db2be412bd7b1139f
Philo van Kemenade <philovankemenade@gmail.com>
2022-02-09 21:28:20 +0100

    update spreadsheet id

--- communities.js
+++ communities.js
@@ -1,14 +1,6 @@
 module.exports = [
-  // {
-  //   "name": "demo",
-  //   "spreadsheetId": "16eV2VLLy1s0JYgL5PWXmw6yxGTVcHt1bnct2cSflx8A"
-  // },
   {
-    name: "mozfest",
-    spreadsheetId: "1V0VW1_6w56V4vgLKfxx1w15MKPdOM0JtAkpvHaEXyXA"
-  },
-  {
-    name: "coee",
-    spreadsheetId: "1KGEbhYWfMMAZx23JvGRhS59v7WzjiyzORrPHNLkdqhw"
+    name: "gftw",
+    spreadsheetId: "1lzSWJc31teXZ9DtsgKmNBViW6_4LBwYpzJlouVwONpE"
   }
 ];
