6c1204b9cdc4281de406d08f8d71617bd217fe74
Philo van Kemenade <philovankemenade@gmail.com>
2022-02-20 03:00:58 +0100

    add key to v-for

--- components/ProfileAvatarList.vue
+++ components/ProfileAvatarList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="flex list p-0 flex-wrap pv1">
-    <li v-for="profileName in profileNames">
+    <li v-for="profileName in profileNames" :key="profileName">
       <ProfileAvatar :profileName="profileName" :class="itemClasses" />
     </li>
   </ul>
