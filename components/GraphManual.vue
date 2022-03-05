296832d0dfa4af8f6f790552052d58641d8569cf
Kevin Cunningham <kevin@kevincunningham.co.uk>
2022-03-05 16:54:41 +0000

    Adds a JS on hover reducer.

--- components/GraphManual.vue
+++ components/GraphManual.vue
@@ -4,12 +4,20 @@
 
 <script>
 
-let sigma
-
 export default {
 	props: {
 		nodes: Array,
 		edges: Array,
+		hoveredNode: {
+			default: undefined,
+			type: Number
+		},
+		hoveredNeighbors: {
+			default() {
+				return undefined
+			},
+			type: Array
+		}
 	},
 	mounted() {
 		const { Sigma } = require("sigma")
@@ -18,12 +26,12 @@ export default {
 
 		const graph = new Graph();
 
-		this.nodes.forEach((node) => console.log(node) || graph.addNode(node.id,
+		this.nodes.forEach((node) => graph.addNode(node.id,
 			{
 				x: Math.floor(Math.random() * 500),
-				y: Math.floor(Math.random() * 500), 
-				size: 7, 
-				label: node.name, 
+				y: Math.floor(Math.random() * 500),
+				size: 7,
+				label: node.name,
 				color: node._cssClass === "Skill" ? "#4ba45b" : "#184eb5"
 			}))
 
@@ -35,7 +43,73 @@ export default {
 			}
 		})
 
-		window.sigma = new Sigma(graph, container)
+		const renderer = new Sigma(graph, container)
+
+		const state = {  };
+
+		function setHoveredNode(node) {
+			if (node) {
+				state.hoveredNode = node;
+				state.hoveredNeighbors = new Set(graph.neighbors(node));
+			} else {
+				state.hoveredNode = undefined;
+				state.hoveredNeighbors = undefined;
+			}
+
+			// Refresh rendering:
+			renderer.refresh();
+		}
+
+		// Bind graph interactions:
+		renderer.on("enterNode", ({ node }) => {
+			setHoveredNode(node);
+		});
+		renderer.on("leaveNode", () => {
+			setHoveredNode(undefined);
+		});
+
+		// Render nodes accordingly to the internal state:
+		// 1. If a node is selected, it is highlighted
+		// 2. If there is query, all non-matching nodes are greyed
+		// 3. If there is a hovered node, all non-neighbor nodes are greyed
+		renderer.setSetting("nodeReducer", (node, data) => {
+			const res = { ...data };
+
+			if (state.hoveredNeighbors && !state.hoveredNeighbors.has(node) && state.hoveredNode !== node) {
+				res.label = "";
+				res.color = "#f6f6f6";
+			}
+
+			if (state.selectedNode === node) {
+				res.highlighted = true;
+			} else if (state.suggestions && !state.suggestions.has(node)) {
+				res.label = "";
+				res.color = "#f6f6f6";
+			}
+
+			return res;
+		});
+
+		// Render edges accordingly to the internal state:
+		// 1. If a node is hovered, the edge is hidden if it is not connected to the
+		//    node
+		// 2. If there is a query, the edge is only visible if it connects two
+		//    suggestions
+		renderer.setSetting("edgeReducer", (edge, data) => {
+			const res = { ...data };
+
+			if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
+				res.hidden = true;
+			}
+
+			if (state.suggestions && (!state.suggestions.has(graph.source(edge)) || !state.suggestions.has(graph.target(edge)))) {
+				res.hidden = true;
+			}
+
+			return res;
+		});
+
+
 	}
 }
 </script>
