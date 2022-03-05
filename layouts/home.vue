d971d452e3552afc595433202cbd56f87ca39703
Philo van Kemenade <philovankemenade@gmail.com>
2022-03-05 17:15:23 +0100

    standardise font

--- layouts/home.vue
+++ layouts/home.vue
@@ -3,24 +3,3 @@
     <nuxt />
   </div>
 </template>
-
-<style>
-html {
-  font-family: "Avenir", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-</style>
