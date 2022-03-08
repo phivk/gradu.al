23cdc99d82ef8a144518a22998a60afcd8dd253f
Philo van Kemenade <philovankemenade@gmail.com>
2022-03-08 23:54:11 +0100

    add test sheet

--- communities.js
+++ communities.js
@@ -1,10 +1,10 @@
 module.exports = [
-  // {
-  //   "name": "demo",
-  //   "spreadsheetId": "16eV2VLLy1s0JYgL5PWXmw6yxGTVcHt1bnct2cSflx8A"
-  // },
   {
-    name: "mozfest",
-    spreadsheetId: "1Y1QE7pZN97QtuDCDfpGqP5c1gMeKmPKGFnSUgtEQZbk",
+    name: "multi sheet test",
+    spreadsheetId: "1UXXi5J2HdulaX15xk6Ylb9qsWYkggI6mlmQorO7rSBo",
   },
+  // {
+  //   name: "mozfest",
+  //   spreadsheetId: "1Y1QE7pZN97QtuDCDfpGqP5c1gMeKmPKGFnSUgtEQZbk",
+  // },
 ];
