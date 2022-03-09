20d272b2211d149cd80e3b6e57ecff977133c0b8
Philo van Kemenade <philovankemenade@gmail.com>
2022-03-09 17:28:15 +0100

    add search bar to filter topics in skills list

--- components/MostPopularSkillsSection.vue
+++ components/MostPopularSkillsSection.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
+  <div class="max-w-2xl mx-auto text-left">
     <slot></slot>
+    <div class="mt-1 relative rounded-md">
+      <input
+        v-model="query"
+        type="text"
+        name="topic"
+        id="topic"
+        class="focus:ring-secondary focus:border-secondary block border-gray-300 rounded-md w-full"
+        placeholder="search for topics"
+      />
+      <div
+        v-show="query"
+        @click="closeQuery"
+        class="absolute inset-y-0 right-0 flex items-center px-3 text-gray-500 cursor-pointer"
+      >
+        <font-awesome-icon :icon="['fas', 'times-circle']" />
+      </div>
+    </div>
     <ul class="list lh-copy max-w-2xl mx-auto">
       <li
         v-for="skill in skillsMaxed"
         :key="skill.name"
-        class="my-2 p-2 rounded bg-white text-black text-opacity-80 font-medium tl"
+        class="my-2 p-2 rounded bg-white text-black text-opacity-80 font-medium tl shadow"
       >
         <div>{{ skill.name }}</div>
         <div class="flex flex-wrap">
@@ -37,13 +54,13 @@
           </div>
         </div>
       </li>
-      <a
-        class="underline hover:no-underline cursor-pointer inline-block my-2"
-        v-show="spillover"
-        @click="maxSkills += step"
-        >show more</a
-      >
     </ul>
+    <a
+      class="underline hover:no-underline cursor-pointer inline-block my-2"
+      v-show="spillover"
+      @click="maxSkills += step"
+      >show more</a
+    >
   </div>
 </template>
 
@@ -54,16 +71,33 @@ export default {
   },
   data() {
     return {
+      maxSkillsDefault: 10,
       maxSkills: 10,
       step: 10,
+      query: "",
     };
   },
   computed: {
+    skillsFiltered() {
+      if (this.query) {
+        return this.skills.filter((skill) =>
+          skill.name.toLowerCase().includes(this.query.toLowerCase())
+        );
+      } else {
+        return this.skills;
+      }
+    },
     skillsMaxed() {
-      return this.skills.slice(0, this.maxSkills);
+      return this.skillsFiltered.slice(0, this.maxSkills);
     },
     spillover() {
-      return Math.max(this.skills.length - this.maxSkills, 0);
+      return Math.max(this.skillsFiltered.length - this.maxSkills, 0);
+    },
+  },
+  methods: {
+    closeQuery() {
+      this.query = "";
+      this.maxSkills = this.maxSkillsDefault;
     },
   },
 };
