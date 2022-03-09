fc4a363c1913173865c7c44fbb2fc3b1b3020e57
Philo van Kemenade <philovankemenade@gmail.com>
2022-03-09 19:13:36 +0100

    update communities for testing

--- communities.js
+++ communities.js
@@ -1,10 +1,10 @@
 module.exports = [
-  {
-    name: "demo",
-    spreadsheetId: "1UXXi5J2HdulaX15xk6Ylb9qsWYkggI6mlmQorO7rSBo",
-  },
   // {
-  //   name: "mozfest",
-  //   spreadsheetId: "1Y1QE7pZN97QtuDCDfpGqP5c1gMeKmPKGFnSUgtEQZbk",
+  //   name: "demo",
+  //   spreadsheetId: "1UXXi5J2HdulaX15xk6Ylb9qsWYkggI6mlmQorO7rSBo",
   // },
+  {
+    name: "mozfest",
+    spreadsheetId: "1Y1QE7pZN97QtuDCDfpGqP5c1gMeKmPKGFnSUgtEQZbk",
+  },
 ];
