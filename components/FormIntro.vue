f2d10c354ea8171072da9128c51276a31d6b8c63
Kevin Cunningham <kevin@kevincunningham.co.uk>
2022-05-28 13:19:03 +0100

    Adds the FormIntro component

---
+++ components/FormIntro.vue
@@ -0,0 +1,19 @@
+<template>
+	<div v-frag>
+		<p class="font-bold">
+			{{ introduction }}
+		</p>
+		<p class="font-light">
+			{{ followup }}
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		introduction: String,
+		followup: String,
+	},
+}
+</script>
