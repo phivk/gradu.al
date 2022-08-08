204af85ecf62de4bd42b695545dc6ceeaa756c79
Philo van Kemenade <philovankemenade@gmail.com>
2022-08-08 17:27:44 +0200

    access form via /take-part

--- components/CTASection.vue
+++ components/CTASection.vue
@@ -43,7 +43,7 @@ export default {
     ctaButtonPrimary: {
       type: Object,
       required: true,
-      default: () => ({ text: "Take part", href: "/join" }),
+      default: () => ({ text: "Take part", href: "/take-part" }),
     },
     ctaButtonSecondary: {
       type: Object,
