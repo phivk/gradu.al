ee4b973ca475659dd88ec4cb21da3222e13332b5
Philo van Kemenade <philovankemenade@gmail.com>
2022-11-14 13:34:57 +0100

    switch community

--- communities.js
+++ communities.js
@@ -3,8 +3,12 @@ module.exports = [
   //   name: "demo",
   //   spreadsheetId: "1UXXi5J2HdulaX15xk6Ylb9qsWYkggI6mlmQorO7rSBo",
   // },
+  // {
+  //   name: "mozfest",
+  //   spreadsheetId: "1Y1QE7pZN97QtuDCDfpGqP5c1gMeKmPKGFnSUgtEQZbk",
+  // },
   {
-    name: "mozfest",
-    spreadsheetId: "1Y1QE7pZN97QtuDCDfpGqP5c1gMeKmPKGFnSUgtEQZbk",
+    name: "gftw",
+    spreadsheetId: "1lzSWJc31teXZ9DtsgKmNBViW6_4LBwYpzJlouVwONpE",
   },
 ];
