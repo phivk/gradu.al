dee5677577e196cb81aecaa7e56ef9ca25dfc177
Philo van Kemenade <philovankemenade@gmail.com>
2022-11-16 17:32:16 +0100

    convert other tables using csv-stringify
    store results in in content/data

--- convert-gsheet-to-supabase.js
+++ convert-gsheet-to-supabase.js
@@ -3,21 +3,68 @@ let edges = require("./content/data/edges.json").edges;
 let skills = require("./content/data/skills.json").skills;
 
 var fs = require("fs");
+const { stringify } = require("csv-stringify");
 
+function writeCSV(arrayOfRows, columns, filePath) {
+  const writableStream = fs.createWriteStream(filePath);
+  const stringifier = stringify({ header: true, columns: columns });
+  arrayOfRows.map((m) => stringifier.write(m));
+  stringifier.pipe(writableStream);
+  console.log("Finished writing data to", filePath);
+}
+
+/* Members */
 var members = nodes
   .filter((n) => n["_cssClass"] == "Member")
   .map((n) => {
-    return [n.id, n.name, n.submittedAt];
+    // return [n.id, n.submittedAt, n.name, null, null];
+    return {
+      id: n.id,
+      created_at: n.submittedAt,
+      username: n.name,
+      email: null,
+      comment: null,
+    };
   });
+writeCSV(
+  members,
+  ["id", "created_at", "username", "email", "comment"],
+  "./content/data/table_members.csv"
+);
 
-let csv = members.join("\n");
+/* Topics */
+var topics = nodes
+  .filter((n) => n["_cssClass"] == "Skill")
+  .map((n) => {
+    return {
+      id: n.id,
+      created_at: n.firstSubmittedOn,
+      name: n.name,
+    };
+  });
+writeCSV(
+  topics,
+  ["id", "created_at", "name"],
+  "./content/data/table_topics.csv"
+);
 
-fs.writeFile("./table-members.csv", csv, "utf8", function (err) {
-  if (err) {
-    console.log(
-      "Some error occured - file either not saved or corrupted file saved."
-    );
-  } else {
-    console.log("It's saved!");
-  }
+/* Members-Topics */
+var m_t_id = 0;
+members_topics = edges.map((edge) => {
+  m_t_id++;
+  let learner = edge.type === "learn" ? edge.sid : null;
+  let topic = edge.tid;
+  let sharer = edge.type === "share" ? edge.sid : null;
+  return {
+    id: m_t_id,
+    created_at: null,
+    learner: learner,
+    topic: topic,
+    sharer: sharer,
+  };
 });
+writeCSV(
+  members_topics,
+  ["id", "created_at", "learner", "topic", "sharer"],
+  "./content/data/table_members_topics.csv"
+);
